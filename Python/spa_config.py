22f00b36fdb1d6c70ebbfe2f0a46043b277732c3
rrov1 <riegerrico@googlemail.com>
2023-01-24 20:54:44 +0100

    Ausgabe http-Responsecodes wenn != 200

--- Python/spa_config.py
+++ Python/spa_config.py
@@ -19,11 +19,17 @@ for nSpaNum in range(len(lSpas)):
     # Do some things with the facade
     # name and id
     print(f"identifier {facade.name}")
-    requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Name?value={urllib.parse.quote(facade.name)}&ack=true")
+    oResponse = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Name?value={urllib.parse.quote(facade.name)}&ack=true")
+    if (oResponse.status_code != 200):
+        print(f"http error: {oResponse.status_code}")
     print(f"identifier {facade.identifier}")
-    requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.ID?value={urllib.parse.quote(facade.identifier)}&ack=true")
+    oResponse = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.ID?value={urllib.parse.quote(facade.identifier)}&ack=true")
+    if (oResponse.status_code != 200):
+        print(f"http error: {oResponse.status_code}")
     print(f"uid: {facade.unique_id}")
-    requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.U_ID?value={urllib.parse.quote(facade.unique_id)}&ack=true")
+    oResponse = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.U_ID?value={urllib.parse.quote(facade.unique_id)}&ack=true")
+    if (oResponse.status_code != 200):
+        print(f"http error: {oResponse.status_code}")
     #
     print(f'Temperatureinheit {facade.water_heater.temperature_unit}')
     requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Temperatureinheit?value={urllib.parse.quote(facade.water_heater.temperature_unit)}&ack=true")
@@ -32,9 +38,13 @@ for nSpaNum in range(len(lSpas)):
     for pump in facade.pumps:
         print(f"{pump.key}")
         print(f"{pump.name}")
-        requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Pumpen.{pump.key}.Name?value={urllib.parse.quote(pump.name)}&ack=true")
-        print(f"{pump.modes}")
-        requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Pumpen.{pump.key}.Modi?value={pump.modes}&ack=true")
+        oRespone = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Pumpen.{pump.key}.Name?value={urllib.parse.quote(pump.name)}&ack=true")
+        if (oResponse.status_code != 200):
+            print(f"http error: {oResponse.status_code}")
+        print(f"{str(pump.modes)}")
+        oResponse = requests.get(f"""{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Pumpen.{pump.key}.Modi?value={urllib.parse.quote(str(pump.modes))}&ack=true""")
+        if (oResponse.status_code != 200):
+            print(f"http error: {oResponse.status_code}")
     #
     print(f"anzahl blowers: {len(facade.blowers)}")
     #
@@ -42,7 +52,9 @@ for nSpaNum in range(len(lSpas)):
     for light in facade.lights:
         print(f"{light.key}")
         print(f"{light.name}")
-        requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Lichter.{light.key}.Name?value={urllib.parse.quote(light.name)}&ack=true")
+        oResponse = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Lichter.{light.key}.Name?value={urllib.parse.quote(light.name)}&ack=true")
+        if (oResponse.status_code != 200):
+            print(f"http error: {oResponse.status_code}")
     #
     print(f"***anzahl Sensoren: {len(facade.sensors)}")
     for sensor in facade.sensors:
@@ -57,7 +69,9 @@ for nSpaNum in range(len(lSpas)):
         sKey = binary_sensor.key
         sKey = sKey.replace(" ", "_")
         sKey = sKey.replace(":", "_")
-        requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Sensoren.{sKey}.Name?value={urllib.parse.quote(binary_sensor.name)}&ack=true")
+        oResponse = requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Sensoren.{sKey}.Name?value={urllib.parse.quote(binary_sensor.name)}&ack=true")
+        if (oResponse.status_code != 200):
+            print(f"http error: {oResponse.status_code}")
     
     print(f"anzahl user devices: {len(facade.all_user_devices)}")
     # user_device brauchen wir nicht, Information doppelt
@@ -67,5 +81,7 @@ for nSpaNum in range(len(lSpas)):
     print(f"anzahl reminders: {len(facade.reminders)}")
     for reminder in facade.reminders:
         print(f"reminder: {reminder}")
-        requests.get(f"{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Erinnerungen.{reminder[0]}?value={urllib.parse.quote(str(reminder[1]))}&ack=true")
+        oResponse = requests.get(f"""{IOBROKER_BASE_URL}javascript.0.Datenpunkte.SwimSpa.{nSpaNum}.Erinnerungen.{reminder[0]}?value={urllib.parse.quote('"' + str(reminder[1]) + '"')}&ack=true""")
+        if (oResponse.status_code != 200):
+            print(f"http error: {oResponse.status_code}")
     
